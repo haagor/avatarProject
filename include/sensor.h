89f74290ac345918bea7d2e40d558f9dde68200e
m-bigglesworth <arnaudgarnier06@gmail.com>
2016-03-23 23:20:36 +0100

    Recopie de code débile...

---
+++ include/sensor.h
@@ -0,0 +1,28 @@
+#ifndef_SENSOR_H_
+#define _SENSOR_H_
+
+#include "OpenNI.h"
+
+enum EActiveStream
+{
+    color_stream,
+    depth_stream1
+};
+
+class CSsensor
+{
+public:
+    openni::Device m_device;
+
+    openni::VideoStream m_colorStream;
+    openni::VideoStream m_depthStream;
+    EActiveStream active_stream;
+
+public:
+    CSensor();
+    ~CSensor();
+
+    bool OnInit(bool show_color_stream);
+};
+
+#endif
