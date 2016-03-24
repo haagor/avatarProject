6de953f2ef36d1fb81cd563ba1b8c1c5719210fd
m-bigglesworth <arnaudgarnier06@gmail.com>
2016-03-24 08:12:15 +0100

    Tentative de résolution de bug de chez Simon

---
+++ build/CMakeFiles/Avatar.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/user/Documents/SI4/S2/RA/Avatar/src/avatar.cpp" "/home/user/Documents/SI4/S2/RA/Avatar/build/CMakeFiles/Avatar.dir/src/avatar.cpp.o"
+  "/home/user/Documents/SI4/S2/RA/Avatar/src/events.cpp" "/home/user/Documents/SI4/S2/RA/Avatar/build/CMakeFiles/Avatar.dir/src/events.cpp.o"
+  "/home/user/Documents/SI4/S2/RA/Avatar/src/gl_objects.cpp" "/home/user/Documents/SI4/S2/RA/Avatar/build/CMakeFiles/Avatar.dir/src/gl_objects.cpp.o"
+  "/home/user/Documents/SI4/S2/RA/Avatar/src/main.cpp" "/home/user/Documents/SI4/S2/RA/Avatar/build/CMakeFiles/Avatar.dir/src/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "UNIX"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/include/SDL"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/CMakeFiles/Avatar.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Avatar.dir/src/main.cpp.o"
+  "CMakeFiles/Avatar.dir/src/avatar.cpp.o"
+  "CMakeFiles/Avatar.dir/src/events.cpp.o"
+  "CMakeFiles/Avatar.dir/src/gl_objects.cpp.o"
+  "Avatar.pdb"
+  "Avatar"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Avatar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
